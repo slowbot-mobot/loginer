52fd08a8a7e54f5296ccc2b8f7201d812de629f1
Adam Tolley <refactorized@gmail.com>
2016-06-24 18:16:59 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'exp_login_development',
-      user:     'refactorized',
+      user:     'postgres',
       password: ''
     },
     pool: {
